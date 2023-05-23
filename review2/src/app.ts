5225167134b497318f3348999e76b33121f7d44a
onionmccabbage <onionmccabbage@live.com>
2023-05-23 15:26:56 +0100

    sample solution for Review 2

--- review2/src/app.ts
+++ review2/src/app.ts
@@ -1,5 +1,4 @@
 import {User, ToDo, Photo} from './types'
-// import {listOfUsers, getListOfPhotos} from './util'
 
 // a method to fetch the data (nb works without async but ts likes async)
 // here the generic type <T> means if we ask for User we get an array of User             | string can be left off
