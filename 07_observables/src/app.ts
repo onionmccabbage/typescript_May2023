573d991ac13f8851831b0301a2477f9ce3e1fc44
onionmccabbage <onionmccabbage@live.com>
2023-05-24 13:56:48 +0100

    working example using observables

--- 07_observables/src/app.ts
+++ 07_observables/src/app.ts
@@ -17,20 +17,36 @@ const testData: string[] = [
 ]
 
 // some utility functions
-const appendResults = (container: HTMLElement, results: string[] | string) => {
+//               testData      what the user typed
+const suggest = (arr:string[], query:string)=>{
+    return arr.filter( (item)=>{
+        // only handle non-empty strings that match the incoming query
+        return query.length >0 && item.startsWith(query)
+    })
+}
+
+const cleanUpUtil = (container:HTMLElement)=>{
+    container.innerHTML = ''
+}
+
+const appendResults = (container: HTMLElement, results: string[]) => { // | string
     // type guard - do we have ONE string or an array of strings?
-    if (typeof (results) == 'string'){
-        //
-    }
-    else {
-        for (const result in results) {
+    // if (typeof (results) == 'string'){
+    //     const li = document.createElement("li")
+    //     const text = document.createTextNode(results)
+    //     li.appendChild(text)
+    //     container.appendChild(li)
+    // }
+    // else {
+        // in returns index, of returns actual value
+        for (const result of results) {
             // creeate an HTML entity for each suggestion
             const li = document.createElement("li")
             const text = document.createTextNode(result)
             li.appendChild(text)
             container.appendChild(li)
         }
-    }
+    // }
 }
 
 // we need to grab bits of the DOM - cast as types so TS can hint
@@ -50,10 +66,20 @@ keyStream$.pipe(
         return input.value
     }),
     tap((item) => {
-        console.log(`we receivd ${item}`)
-    })
+        // currently the entire content of the 'input' element (its value)
+        console.log(`we received ${item}`)
+        return item
+    }),
+    map( (query)=>{
+        // this will ALWAYS return an array
+        return suggest(testData, query)
+    } )
     // NB nothing is instantiated until we have at least one subscriber
-).subscribe((result) => { appendResults(results, result) })
+).subscribe((result) => { 
+    // remove any previous suggestions
+    cleanUpUtil(results)
+    // inject the curent suggestion(s)
+    appendResults(results, result) })
 
 
 
